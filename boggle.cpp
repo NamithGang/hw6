1ecf75d31a6eebae5307b72546ebcf60ebd20bc0
Namith Gangireddyvari <gangired@usc.edu>
2025-05-05 06:29:33 +0000

    Removed tests

--- boggle.cpp
+++ boggle.cpp
@@ -95,5 +95,22 @@ bool boggleHelper(const std::set<std::string>& dict, const std::set<std::string>
 								   std::string word, std::set<std::string>& result, unsigned int r, unsigned int c, int dr, int dc)
 {
 //add your solution here!
-
+	if(r >= board.size() || c >= board[r].size()){
+    if(dict.find(word) != dict.end()) {
+      result.insert(word);
+      return true;
+    }
+    return false;
+  }
+  word += board[r][c];
+  bool largerWord = false;
+  bool canExtend = prefix.find(word) != prefix.end();
+  if(canExtend){
+    largerWord = boggleHelper(dict,prefix,board,word,result,r+dr,c+dc,dr,dc);
+  }
+  if(!largerWord && dict.find(word) != dict.end()){
+    result.insert(word);
+    return true;
+  }
+  return largerWord;
 }
--- hash.h
+++ hash.h
@@ -20,15 +20,42 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+        unsigned long long wValues[5] = {0};
+        int arrIndex = 4;
+        int index = k.size()-1;
 
+        while(index >= 0 && arrIndex >= 0){
+          unsigned long long sum = 0;
+          unsigned long long power = 1;
 
+          for(int i = 0; i < 6 && index >= 0; i++){
+            sum += power*letterDigitToNumber(k[index]);
+            index--;
+            power*=36;
+          }
+
+          wValues[arrIndex--] = sum;
+        }
+
+        HASH_INDEX_T output = 0;
+        for(int i = 0; i < 5; i++){
+          output += rValues[i]*wValues[i];
+        }
+        return output;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
-
+      if(letter >= '0' && letter <= '9'){
+        return (letter - '0') + 26;
+      } else if (letter >= 'A' && letter <= 'Z'){
+        return (letter - 'A');
+      } else if (letter >= 'a' && letter <= 'z'){
+        return (letter - 'a');
+      }
+      return 0;
     }
 
     // Code to generate the random R values
